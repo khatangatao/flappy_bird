f52b7c7e1985dd66f7ceb448130d8a2a00714121
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2021-02-22 17:04:03 +0700

    first commit

---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dl) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = FlappyDemo.HEIGHT;
+		config.width = FlappyDemo.WIDTH;
+
+		config.title = FlappyDemo.TITLE;
+
+
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
